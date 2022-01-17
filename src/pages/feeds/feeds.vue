cd891137b6146192b2be708eeee2b67f395b7677
VdNeretin <88381432+VdNeretin@users.noreply.github.com>
2022-01-17 10:19:11 +0300

    Gitogramm V1

---
+++ src/pages/feeds/feeds.vue
@@ -0,0 +1,58 @@
+<template>
+  <div class="topline">
+    <topline>
+      <template #headline>
+      </template>
+      <template #content>
+        <ul class="stories">
+          <li class="stories-item" v-for="story in stories" :key="story.id">
+            <story-user-item
+        :avatar="story.avatar"
+        :username="story.username"
+        @onPress="handlePress(story.id)"
+        />
+          </li>
+        </ul>
+      </template>
+      <template>
+      </template>
+    </topline>
+  </div>
+  <div class="c-feed">
+        <toggler @onToggle="toggle" />
+        <div class="comments" v-if="shown">
+          <ul class="comments-list">
+            <li class="comments-" v-for="n in 5" :key="n">
+              <comment text="Some text" username="Hohn Doe" />
+              </li>
+          </ul>
+          </div>
+      </div>
+</template>
+
+<script>
+import { topline } from '../../components/topline'
+import { storyUserItem } from '../../components/storyUserItem'
+import { toggler } from '../../components/toggler'
+import { comment } from '../../components/comment'
+import stories from './data.json'
+export default {
+  name: 'feeds',
+  components: {
+    topline, storyUserItem, toggler, comment
+  },
+  data () {
+    return {
+      stories,
+      shown: false
+    }
+  },
+  methods: {
+    toggle (isOpened) {
+      this.shown = isOpened
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped src="./feeds.scss"></style>
